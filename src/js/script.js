fd629dde528a07d9cdd050647c1f805f7baa8b21
Robert Palczewski <robert.palczewski@gmail.com>
2019-08-10 17:36:30 +0200

    Add generate HTML for product

--- src/js/script.js
+++ src/js/script.js
@@ -53,21 +53,59 @@
   };
 
   class Product{
-    constructor(){
+    constructor(id, data){
       const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      console.log('new Product: ', thisProduct);
+    }
+    renderInMenu(){
+      const thisProduct = this;
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
 
-      console.log('new Product:', thisProduct);
+      /* create element using utils.createElementFromHTML */
+
+      /*find menu container */
+
+      /* add element to menu */
+
+
     }
   }
 
+
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      console.log('thisApp.data: ', thisApp.data);
+
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+      /*const testProduct = new Product();
+      console.log('testProduct: ', testProduct);*/
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
+
       console.log('*** App starting ***');
       console.log('thisApp:', thisApp);
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
