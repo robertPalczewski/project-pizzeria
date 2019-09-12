12bc679b0a5fa2f50721773aa4107ad33ba4aa08
Robert Palczewski <robert.palczewski@gmail.com>
2019-09-12 19:54:21 +0200

    Add activate of subpages

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,68 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    // eslint-disable-next-line no-unused-vars
+    let pageMatchingHash = thisApp.pages[0].id;
+    for(let page of thisApp.pages) {
+      if(page.id === idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(idFromHash);
+
+    for(let link of thisApp.navLinks) {
+      link.addEventListener('click', function(event) {
+        event.preventDefault();
+        const clickedElement = this;
+
+        /* get page id form href attribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+      });
+    }
+
+  },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages) {
+
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+
+      /*if(page.id === pageId) {
+        page.classList.add(classNames.pages.active);
+      } else {
+        page.classList.remove(classNames.pages.active);
+      }*/
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks) {
+
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') === '#' + pageId
+      );
+    }
+  },
+
   initMenu: function () {
     const thisApp = this;
     console.log('thisApp.data: ', thisApp.data);
@@ -44,6 +104,8 @@ const app = {
 
   initCart: function () {
     const thisApp = this;
+
+    thisApp.initPages();
 
     const cartElem = document.querySelector(select.containerOf.cart);
     thisApp.cart = new Cart(cartElem);
